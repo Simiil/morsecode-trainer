19b5615e8a59d2fde219645a7dfbdef2b9d01058
Gregor Volkmann <mail@volkmann.me>
2016-10-19 16:31:48 +0200

    remove example code from tutorial

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -3,14 +3,11 @@
 // Declare app level module which depends on views, and components
 angular.module('morsecodeTrainer', [
   'ngRoute',
-  'morsecodeTrainer.view1',
-  'morsecodeTrainer.view2',
-  'morsecodeTrainer.auth',
   'morsecodeTrainer.trainer'
   // 'morsecodeTrainer.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/auth'});
+  $routeProvider.otherwise({redirectTo: '/trainer'});
 }]);
