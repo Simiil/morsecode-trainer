5855b35ab4e9f175f19c8b4bdcb797c666868977
Johannes Höß <johannes@hoess-ottersweier.de>
2016-10-29 17:01:11 +0200

    merge

--- src/main/webapp/view_trainer/trainer.js
+++ src/main/webapp/view_trainer/trainer.js
@@ -10,82 +10,93 @@ angular.module('morsecodeTrainer.trainer', ['ngRoute'])
 }])
 
 .controller('TrainerCtrl', ['$scope', '$http', function($scope, $http) {
-  $scope.challenge = '---';
-  $scope.itu = {
-    A: ".-",
-    B: "-...",
-    C: "-.-.",
-    D: "-..",
-    E: ".",
-    F: "..-.",
-    G: "--.",
-    H: "....",
-    I: "..",
-    J: ".---",
-    K: "-.-",
-    L: ".-..",
-    M: "--",
-    N: "-.",
-    O: "---",
-    P: ".--.",
-    Q: "--.-",
-    R: ".-.",
-    S: "...",
-    T: "-",
-    U: "..-",
-    V: "...-",
-    W: ".--",
-    X: "-..-",
-    Y: "-.--",
-    Z: "--..",
 
-    1: ".----",
-    2: "..---",
-    3: "...--",
-    4: "....-",
-    5: ".....",
-    6: "-....",
-    7: "--...",
-    8: "---..",
-    9: "----.",
-    0: "-----",
-  };
-  $scope.alphabet = $scope.itu;
 
-  $scope.getChallenge = function(){
-      $http.get("/api/morse/game/gettone/foo").then(function(response) {
-          $scope.aktChallenge = response.data;
-          console.log($scope.aktChallenge);
-      });
-  }
+        $scope.challenge = '---';
+        $scope.itu = {
+        A: ".-",
+        B: "-...",
+        C: "-.-.",
+        D: "-..",
+        E: ".",
+        F: "..-.",
+        G: "--.",
+        H: "....",
+        I: "..",
+        J: ".---",
+        K: "-.-",
+        L: ".-..",
+        M: "--",
+        N: "-.",
+        O: "---",
+        P: ".--.",
+        Q: "--.-",
+        R: ".-.",
+        S: "...",
+        T: "-",
+        U: "..-",
+        V: "...-",
+        W: ".--",
+        X: "-..-",
+        Y: "-.--",
+        Z: "--..",
 
-  $scope.putChallenge = function(character){
+        1: ".----",
+        2: "..---",
+        3: "...--",
+        4: "....-",
+        5: ".....",
+        6: "-....",
+        7: "--...",
+        8: "---..",
+        9: "----.",
+        0: "-----",
+        };
+        $scope.alphabet = $scope.itu;
 
-      var req = {
-         method: 'PUT',
-         url: '/api/morse/game/gettone/foo',
-         headers: {
-           'Content-Type': "application/json"
-         },
-         data: {
-            	"character": character,
-              	"id": $scope.aktChallenge.id
+        $scope.getChallenge = function(){
+          $http.get("/api/morse/game/gettone/foo").then(function(response) {
+              $scope.aktChallenge = response.data;
+              console.log($scope.aktChallenge);
+          });
+      }
+
+      $scope.putChallenge = function(character){
+
+          var req = {
+             method: 'PUT',
+             url: '/api/morse/game/gettone/foo',
+             headers: {
+               'Content-Type': "application/json"
+             },
+             data: {
+                	"character": character,
+                  	"id": $scope.aktChallenge.id
+                }
             }
-        }
 
-        var responseData;
+            var responseData;
 
-        $http(req).then(function(response) {
-            console.log("Sending Successful");
-            responseData = response.data;
-            console.log(responseData);
-            return responseData;
-        });
-  }
+            $http(req).then(function(response) {
+                console.log("Sending Successful");
+                responseData = response.data;
+                console.log(responseData);
+                return responseData;
+            });
+      }
 
-  $scope.setChallenge = function(letter) {
-    $scope.lastChallenge =  $scope.putChallenge(letter);
-    $scope.challenge = $scope.itu[letter] + $scope.lastChallenge;
-    $scope.getChallenge();
-  };
+      $scope.setChallenge = function(letter) {
+        $scope.lastChallenge =  $scope.putChallenge(letter);
+        $scope.challenge = $scope.itu[letter] + $scope.lastChallenge;
+        $scope.getChallenge();
+      };
+
+
+      $scope.onInit = function(){
+          $scope.getChallenge().then(function(response){
+                $scope.challenge = response.data.character.tone;
+                console.log("hier wurden daten abgefragt!");
+          });
+      };
+      $scope.onInit();
 }]);
