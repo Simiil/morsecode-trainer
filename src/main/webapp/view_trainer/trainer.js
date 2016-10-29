4c6b66482faab13ad50feba55a9e7e642d288d76
Gregor Volkmann <mail@volkmann.me>
2016-10-29 18:36:47 +0200

    beautify trainer.js

--- src/main/webapp/view_trainer/trainer.js
+++ src/main/webapp/view_trainer/trainer.js
@@ -3,16 +3,13 @@
 angular.module('morsecodeTrainer.trainer', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/trainer', {
-    templateUrl: 'view_trainer/trainer.html',
-    controller: 'TrainerCtrl'
-  });
+    $routeProvider.when('/trainer', {
+        templateUrl: 'view_trainer/trainer.html',
+        controller: 'TrainerCtrl'
+    });
 }])
 
 .controller('TrainerCtrl', ['$scope', '$http', function($scope, $http) {
-
-
-    // $scope.challenge = '---';
     $scope.itu = {
         A: ".-",
         B: "-...",
@@ -52,84 +49,86 @@ angular.module('morsecodeTrainer.trainer', ['ngRoute'])
         9: "----.",
         0: "-----",
     };
+
     $scope.alphabet = $scope.itu;
 
-      $scope.dropDownMenus = {};
-      $scope.dropDownMenus = {
-          "code": {
-              "ITU": "ITU",
-              "Morse": "Morse",
-              "Gerke": "Gerke"
-          },
-          "speed": {
-              1: "fa fa-wheelchair",
-              2: "fa fa-bicycle",
-              3: "fa fa-car",
-              4: "fa fa-plane",
-              5: "fa fa-rocket"
-          },
-          "noise": {
-              1: "fa fa-circle",
-              2: "fa fa-certificate",
-              3: "fa fa-asterisk"
-          }
-      };
+    $scope.dropDownMenus = {};
+    $scope.dropDownMenus = {
+        "code": {
+            "ITU": "ITU",
+            "Morse": "Morse",
+            "Gerke": "Gerke"
+        },
+        "speed": {
+            1: "fa fa-wheelchair",
+            2: "fa fa-bicycle",
+            3: "fa fa-car",
+            4: "fa fa-plane",
+            5: "fa fa-rocket"
+        },
+        "noise": {
+            1: "fa fa-circle",
+            2: "fa fa-certificate",
+            3: "fa fa-asterisk"
+        }
+    };
 
-      $scope.dropDownCodeActive = $scope.dropDownMenus["code"]["ITU"];
-      $scope.dropDownSpeedActive = $scope.dropDownMenus["speed"][1];
-      $scope.dropDownNoiseActive = $scope.dropDownMenus["noise"][1];
-      $scope.codeVisibilityStatus = true;
-      $scope.sheetVisibilityStatus = true;
+    $scope.dropDownCodeActive = $scope.dropDownMenus["code"]["ITU"];
+    $scope.dropDownSpeedActive = $scope.dropDownMenus["speed"][1];
+    $scope.dropDownNoiseActive = $scope.dropDownMenus["noise"][1];
+    $scope.codeVisibilityStatus = true;
+    $scope.sheetVisibilityStatus = true;
 
-      $scope.getChallenge = function(){
-          $http.get("/api/morse/game/gettone/foo").then(function(response) {
-              $scope.aktChallenge = response.data;
-              console.log($scope.aktChallenge);
-              console.log($scope.aktChallenge.character.tone[0].toString());
-              console.log("false");
-              return $scope.aktChallenge.character.tone[0].toString();
-          });
-      }
+    $scope.getChallenge = function() {
+        $http.get("/api/morse/game/gettone/foo").then(function(response) {
+            $scope.aktChallenge = response.data;
+            console.log($scope.aktChallenge);
+            console.log($scope.aktChallenge.character.tone[0].toString());
+            console.log("false");
+            return $scope.aktChallenge.character.tone[0].toString();
+        });
+    }
 
-      $scope.putChallenge = function(character){
+    $scope.putChallenge = function(character) {
 
-          var req = {
-             method: 'PUT',
-             url: '/api/morse/game/gettone/foo',
-             headers: {
-               'Content-Type': "application/json"
-             },
-             data: {
-                	"character": character,
-                  	"id": $scope.aktChallenge.id
-                }
+        var req = {
+            method: 'PUT',
+            url: '/api/morse/game/gettone/foo',
+            headers: {
+                'Content-Type': "application/json"
+            },
+            data: {
+                "character": character,
+                "id": $scope.aktChallenge.id
             }
+        }
 
-            var responseData;
+        var responseData;
 
-            $http(req).then(function(response) {
-                console.log("Sending Successful");
-                responseData = response.data;
-                console.log(responseData);
-                return responseData;
-            });
-      }
+        $http(req).then(function(response) {
+            console.log("Sending Successful");
+            responseData = response.data;
+            console.log(responseData);
+            return responseData;
+        });
+    }
 
-      $scope.setChallenge = function(letter) {
-        $scope.lastChallenge =  $scope.putChallenge(letter);
+    $scope.setChallenge = function(letter) {
+        $scope.lastChallenge = $scope.putChallenge(letter);
         $scope.challenge = $scope.itu[letter] + $scope.lastChallenge;
         $scope.getChallenge();
-      };
+    };
 
-
-      $scope.onInit = function(){
-          $scope.challenge = $scope.getChallenge();
+    $scope.onInit = function() {
+        $scope.challenge = $scope.getChallenge();
         //   $scope.challenge = "$scope.getChalleng";
         //   $scope.getChallenge().then(function(response){
         //         console.log("hier wurden daten abgefragt!");
         //   });
-      };
-      $scope.onInit();
+    };
+
+    $scope.onInit();
+
     $scope.setChallenge = function(letter) {
         $scope.challenge = $scope.itu[letter];
     };
